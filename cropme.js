e78aa792eb85adfb63d6419752c355271718e5a1
shpontex <iMac-de-Khereddine.local>
2018-12-01 13:17:49 +0900

    add ellipse

--- cropme.js
+++ cropme.js
@@ -1,92 +1,86 @@
 (function (global) {
-  // ----------------- Pollyfils ---------------------------
 
-  var o, p
+    var o, p
 
-  function transform() {
-    return 'translate(' + p.x + 'px,' + p.y + 'px) scale(' + p.scale + ') rotate(' + p.deg + 'deg)'
-  }
-
-  function createContext() {
-    createContainer.call(this)
-    createSlider.call(this)
-    const image = p.image = document.createElement('img')
-    image.ondragstart = () => false
-    const boundary = p.boundary = document.createElement('div')
-
-    o.boundary.width = o.boundary.width > o.container.width ? o.container.width : o.boundary.width
-    o.boundary.height = o.boundary.height > o.container.height ? o.container.height : o.boundary.height
-    boundary.style.width = o.boundary.width + 'px'
-    boundary.style.height = o.boundary.height + 'px'
-    boundary.className = 'boundary'
-
-    if (o.boundary.type === 'circle') {
-      boundary.className = 'boundary circle'
+    function transform() {
+      return 'translate(' + p.x + 'px,' + p.y + 'px) scale(' + p.scale + ') rotate(' + p.deg + 'deg)'
     }
 
-    p.container.appendChild(image)
-    p.container.appendChild(boundary)
+    function createContext() {
+      createContainer.call(this)
+      createSlider.call(this)
+      const image = p.image = document.createElement('img')
+      image.ondragstart = () => false
+      const boundary = p.boundary = document.createElement('div')
 
-    let boundary_border = (o.container.width - o.boundary.width) / 2
-    let border = boundary_border < p.boundary.clientLeft ? boundary_border : p.boundary.clientLeft
+      o.boundary.width = o.boundary.width > o.container.width ? o.container.width : o.boundary.width
+      o.boundary.height = o.boundary.height > o.container.height ? o.container.height : o.boundary.height
+      boundary.style.width = o.boundary.width + 'px'
+      boundary.style.height = o.boundary.height + 'px'
+      boundary.className = 'boundary'
 
-    p.boundary.style.borderWidth = border + 'px'
-    p.boundary.border = border
-
-    let x, movex = 0,
-      y, movey = 0
-
-    let mousemove = function (e) {
-      p.x = e.pageX - x
-      p.y = e.pageY - y
-      image.style.transform = transform()
-    }
-    image.addEventListener('mousedown', function (e) {
-      movex = p.x || movex
-      movey = p.y || movey
-      x = e.pageX - movex
-      y = e.pageY - movey
-      window.addEventListener('mousemove', mousemove)
-    })
-
-    document.addEventListener('mouseup', function (e) {
-      window.removeEventListener('mousemove', mousemove);
-    })
-
-    p.slider.addEventListener('input', function (e) {
-      p.scale = parseFloat(e.target.value)
-      image.style.transform = transform()
-    })
-    let mousewheel = function (e) {
-      e.preventDefault()
-      p.scale = p.slider.value = p.scale + (e.wheelDelta / 1200 * p.scale)
-      image.style.transform = transform()
-    }
-    p.container.addEventListener('mousewheel', mousewheel)
-
-  }
-
-  function createCanvas(options) {
-    let canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d');
-
-    let width = o.boundary.width
-    let height = o.boundary.height
-
-    if (typeof options === 'object') {
-      if (options.scale) {
-        width = width * options.scale
-        height = height * options.scale
-      } else {
-        if (options.size && options.size.width) {
-          width = options.size.width
-        }
-        if (options.size && options.size.height) {
-          height = options.size.height
-        }
+      if (o.boundary.type === 'circle') {
+        boundary.className = 'boundary circle'
       }
 
+      p.container.appendChild(image)
+      p.container.appendChild(boundary)
+
+      let boundary_border = (o.container.width - o.boundary.width) / 2
+      let border = boundary_border < p.boundary.clientLeft ? boundary_border : p.boundary.clientLeft
+
+      p.boundary.style.borderWidth = border + 'px'
+      p.boundary.border = border
+
+      let x, movex = 0,
+        y, movey = 0
+
+      let mousemove = function (e) {
+        p.x = e.pageX - x
+        p.y = e.pageY - y
+        image.style.transform = transform()
+      }
+      image.addEventListener('mousedown', function (e) {
+        movex = p.x || movex
+        movey = p.y || movey
+        x = e.pageX - movex
+        y = e.pageY - movey
+        window.addEventListener('mousemove', mousemove)
+      })
+
+      document.addEventListener('mouseup', function (e) {
+        window.removeEventListener('mousemove', mousemove);
+      })
+
+      p.slider.addEventListener('input', function (e) {
+        p.scale = parseFloat(e.target.value)
+        image.style.transform = transform()
+      })
+      let mousewheel = function (e) {
+        e.preventDefault()
+        p.scale = p.slider.value = p.scale + (e.wheelDelta / 1200 * p.scale)
+        image.style.transform = transform()
+      }
+      p.container.addEventListener('mousewheel', mousewheel)
+
     }
+
+    function createCanvas(options) {
+      let canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+
+      let width = o.boundary.width
+      let height = o.boundary.height
+
+      if (typeof options === 'object') {
+        if (options.scale) {
+          width = width * options.scale
+          height = height * options.scale
+        } else if (options.width) {
+          height = options.width * height / width
+          width = options.width
+        }
+      }
 
     canvas.width = width
     canvas.height = height
@@ -126,7 +120,17 @@
       ctx.translate(width / 2, height / 2);
       ctx.rotate(-deg * Math.PI / 180);
       ctx.translate(-width / 2, -height / 2);
-      ctx.translate((p.x - nx) * xs, (p.y - ny) * ys)
+      ctx.scale(1, o.boundary.height / o.boundary.width);
+      let diff = (o.boundary.width - o.boundary.height) / 2 * xs
+      let x_coordinate = (p.x - nx) * xs
+      let y_coordinate = (p.y - ny) * ys
+      if (diff > 0) {
+        y_coordinate = diff + y_coordinate * 2
+      } else if (diff < 0) {
+        y_coordinate = diff + y_coordinate / 2
+      }
+      ctx.translate(x_coordinate, y_coordinate)
+
       ctx.globalCompositeOperation = 'destination-in'
       ctx.arc(width / 2, height / 2, width / 2, 0, 2 * Math.PI)
       ctx.fill();
