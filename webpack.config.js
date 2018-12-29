6c41928910c4141296240739e0f18c65a4eaab5c
shpontex <radowan@owndays.co.jp>
2018-12-29 12:57:52 +0900

    remove watch from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const devMode = process.env.NODE_ENV !== 'production'
 
 const { BannerPlugin } = require('webpack');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -13,7 +12,6 @@ module.exports = {
   output: {
     filename: 'cropme.js',
   },
-  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "cropme.css",
